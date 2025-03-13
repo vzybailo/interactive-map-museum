5ccc199793d5b27bd9cc3cde421e550a46b0c16e
Vitali Zybaila <vental@bk.ru>
2025-03-13 15:28:31 -0500

    add tooltips

---
+++ scripts.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("infoModal");
+    const modalText = document.getElementById("modal-text");
+    const closeModal = document.querySelector(".close");
+
+    document.querySelectorAll(".info-block").forEach(block => {
+        block.addEventListener("click", function () {
+            const info = this.getAttribute("data-info");
+            modalText.textContent = info;
+            modal.style.display = "flex";
+        });
+    });
+
+    closeModal.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+});
