3e7d4ded6a0346157c4a7531576fad9904cfd13f
Vitali Zybaila <vental@bk.ru>
2025-03-17 15:21:15 -0500

    mobile version

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const modal = document.getElementById("infoModal");
-    const modalText = document.getElementById("modal-text");
-    const closeModal = document.querySelector(".close");
+    const modal = document.getElementById("infoModal")
+    const modalText = document.getElementById("modal-text")
+    const closeModal = document.querySelector(".close")
 
     document.querySelectorAll(".info-block").forEach(block => {
         block.addEventListener("click", function () {
-            const info = this.getAttribute("data-info");
-            modalText.textContent = info;
-            modal.style.display = "block";
-        });
-    });
+            const info = this.getAttribute("data-info")
+            modalText.textContent = info
+            modal.style.display = "block"
+        })
+    })
 
     closeModal.addEventListener("click", function () {
-        modal.style.display = "none";
-    });
+        modal.style.display = "none"
+    })
 
     window.addEventListener("click", function (event) {
         if (event.target === modal) {
-            modal.style.display = "none";
+            modal.style.display = "none"
         }
-    });
-});
+    })
+})
